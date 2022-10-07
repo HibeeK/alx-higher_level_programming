192ea9b3da2a629348b11f306e26c4b8e40c9172
HibeeK <akintolauser@gmail.com>
2022-10-07 00:22:51 +0000

    fied

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from sys import argv
     if len(argv) != 4:
         print("Usage: ./100-my_caalculator.py <a> <operator> <b>")
         quit(1)
     a = int(argv[1])
     b = int(argv[3])
     ops = ["+", "-", "*", "/"]
-    from calculator_1 import add, sud, mul, div
+    from calculator_1 import add, sub, mul, div
     funcs = [add, sub, mul, div]
     for i, s in enumerate(ops):
         if argv[2] == s:
